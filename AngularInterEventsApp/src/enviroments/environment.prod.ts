f6b242554e2492e0f2ffa10c38ae25e24fc0ea75
camilo nova <camiloandres1419@gmail.com>
2024-11-27 02:17:18 -0500

    commit

---
+++ AngularInterEventsApp/src/enviroments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    urlApiEvents: 'http://www.interevento.somee.com/api/Events',
+    urlApiUsers: 'http://www.interevento.somee.com/api/Users',
+    urlApiAttendant: 'http://www.interevento.somee.com/api/Attendant',
+    idUsuario: '',
+};
